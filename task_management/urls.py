3da546e56b2de8fa8e224c400a33d67a0a0092d3
mashukabashar <diyamoni422@gmail.com>
2025-03-26 19:19:25 +0600

    first-project-done

---
+++ task_management/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from tasks.views import home
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", home),
+    path("tasks/", include("tasks.urls"))
+]+debug_toolbar_urls()
