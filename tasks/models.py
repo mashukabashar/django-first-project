0929799472a771ede064f7af9c0821637d894a6c
mashukabashar <diyamoni422@gmail.com>
2025-05-03 19:46:40 +0600

    last

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -29,7 +28,6 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # details
 
     def __str__(self):
         return self.title
@@ -49,13 +47,13 @@ class TaskDetail(models.Model):
         on_delete=models.CASCADE,
         related_name='details'
     )
-    # assigned_to = models.CharField(max_length=100)
+    assigned_to = models.ManyToManyField(Employee)  # ManyToManyField for employees
     priority = models.CharField(
         max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"Details form Task {self.task.title}"
+        return f"Details for Task {self.task.title}"
 
 
 class Project(models.Model):
