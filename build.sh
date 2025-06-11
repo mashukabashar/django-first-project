7a6a7a71f6d37bf78815a70ba71a5eeca4b172d4
mashukabashar <diyamoni422@gmail.com>
2025-06-11 10:03:38 +0600

    mashuka

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Apply database migrations
-python manage.py migrate
-
-# Collect static files into STATIC_ROOT
-python manage.py collectstatic --no-input
--- task_management/settings.py
+++ task_management/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-b3=w=m+=b=fga^&9+98ohh%-@gqrvtjh2=hve9h$b27*jdptj3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
+ALLOWED_HOSTS = []
+# CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
@@ -151,19 +151,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
-# STATICFILES_DIRS=[
-#     BASE_DIR/'static'
-# ]
-
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # This is your custom static folder
+STATICFILES_DIRS=[
+    BASE_DIR/'static'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',  # This is your custom static folder
+# ]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
