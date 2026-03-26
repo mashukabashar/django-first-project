65cc92d4bdacc3c322498e46077e8d88b25294ae
mashukabashar <diyamoni422@gmail.com>
2026-03-26 16:14:23 +0600

    env variable added module 11.4

---
+++ tasks/signals.py
@@ -0,0 +1,4 @@
+from django.db.models.signals import post_save, pre_save, m2m_changed, post_delete
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from tasks.models import Task
